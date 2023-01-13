9b81ada4776726077691bd994bc7e2e6e18962bb
AlenaNovik <elena.pigaleva@gmail.com>
2023-01-13 15:40:12 +0300

    feat: jest crud test added

---
+++ src/test/mockData.ts
@@ -0,0 +1,16 @@
+export const mockData = {
+  user: {
+    username: "testUser",
+    age: 10,
+    hobbies: [""],
+  },
+  updatedUser: {
+    username: "testUser",
+    age: 25,
+    hobbies: ["test_hobby"],
+  },
+  invalidUserData: {
+    age: 18,
+    hobbies: ["test_hobby"],
+  },
+};
